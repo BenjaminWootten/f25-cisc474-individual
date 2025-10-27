3ac5ca4fc7ec2001f5e597b4bf9a29bd390fc3bd
Ben Wootten <bwootten@udel.edu>
2025-10-27 03:05:42 -0400

    added sample courses to dashboard

--- apps/web-start/src/routes/dashboard/index.tsx
+++ apps/web-start/src/routes/dashboard/index.tsx
@@ -1,9 +1,19 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router';
+import { DashboardCourse } from '../../components/DashboardCourse';
+import './dashboard.css';
 
 export const Route = createFileRoute('/dashboard/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/dashboard"!</div>
+  return (
+  <div>
+    <h1>Courses</h1>
+    <div className='listContainer'>
+      <DashboardCourse id='1' code='CISC474' title='Advanced Web Technologies' instructor='Dr. Bart' grade='90%'></DashboardCourse>
+      <DashboardCourse id='2' code='MATH349' title='Linear Algebra' instructor='Dr. Park' grade='80%'></DashboardCourse>
+    </div>
+  </div>
+  );
 }
