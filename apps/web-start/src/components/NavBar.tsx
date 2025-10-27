9201ad8a3fdfc9636b23f04eabf4102141eb02c4
Ben Wootten <bwootten@udel.edu>
2025-10-26 23:35:52 -0400

    styled navbar

--- apps/web-start/src/components/NavBar.tsx
+++ apps/web-start/src/components/NavBar.tsx
@@ -1,13 +1,29 @@
 import { Link } from '@tanstack/react-router';
 import './NavBar.css';
+import { link } from 'fs';
+
+type Link = {to: string, label: string}
+
+const links = [
+  {to: '/home', label: 'Home', exact: true},
+  {to: '/dashboard', label: 'Dashboard', exact: true},
+  {to: '/assignments', label: 'Assignments', exact: true},
+  {to: '/profile', label: 'Profile', exact: true},
+]
 
 export function NavBar() {
     return(
-        <div className="navbar">
-            <Link to="/home" className="button">Home</Link>
-            <Link to="/dashboard" className="button">Dashboard</Link>
-            <Link to="/assignments" className="button">Assignments</Link>
-            <Link to="/profile" className="button">Profile</Link>
+        <div id="navbar">
+            {links.map((link: {to: string, label: string, exact: boolean}) => (
+                <Link
+                to={link.to}
+                className="tab"
+                // Code from chatgpt
+                activeProps={{ 'aria-current': 'page' }}
+                activeOptions={{ exact: link.exact, includeHash: false, includeSearch: false }}
+                // end
+                >{link.label}</Link>
+            ))}
         </div>
     )
 }
