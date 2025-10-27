664fc12c959b851182ebe073360214b402bda9bf
Ben Wootten <bwootten@udel.edu>
2025-10-26 21:19:03 -0400

    navbar styling

--- apps/web-start/src/components/NavBar.tsx
+++ apps/web-start/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { Link } from '@tanstack/react-router';
+import './NavBar.css';
 
 export function NavBar() {
     return(
         <div className="navbar">
-            <p>navbar</p>
-            <Link to="/">Home</Link>
-            <Link to="/dashboard">dashboard</Link>
+            <Link to="/home" className="button">Home</Link>
+            <Link to="/dashboard" className="button">Dashboard</Link>
+            <Link to="/assignments" className="button">Assignments</Link>
+            <Link to="/profile" className="button">Profile</Link>
         </div>
     )
 }
