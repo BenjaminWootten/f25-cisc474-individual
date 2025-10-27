7ab50620500c1c2cb7054305f1a5e053b92a15c7
Ben Wootten <bwootten@udel.edu>
2025-10-27 03:05:59 -0400

    added props to populate course info

--- apps/web-start/src/components/DashboardCourse.tsx
+++ apps/web-start/src/components/DashboardCourse.tsx
@@ -1,19 +1,23 @@
 import './DashboardCourse.css';
+import React from 'react';
+import { Link } from '@tanstack/react-router';
 
 type Course = {
     id: string;
     code: string;
     title: string;
     instructor: string;
-    grade: number;
+    grade: string;
 }
 
-export function DashboardCourse() {
+export function DashboardCourse({id, code, title, instructor, grade}: Course): React.JSX.Element {
     return (
-        <div className="container">
-            <div className="item"><p className="title">code: title</p></div>
-            <div className="item"><p>instructor</p></div>
-            <div className="item grade"><p>94%</p></div>
-        </div>
+        <Link to="/home">
+            <div className="courseContainer">
+                <div className="item title"><p className="title">{code}: {title}</p></div>
+                <div className="item"><p>{instructor}</p></div>
+                <div className="item grade"><p>{grade}</p></div>
+            </div>
+        </Link>
     )
 }
